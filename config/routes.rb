ef8e7d2e7ec211df074a0607ce494f7cb5f2bb7c
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-02-24 17:00:02 +0000

    added index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'plants', to: 'plants#index'
+  get 'plants/:id', to: 'plants#show'
 end
