0461f22c80289a5a9e653a35c10bb37af3f8b52f
Jessica DeWitt <jessicaadewitt@gmail.com>
2020-02-27 10:55:56 +0000

    got logout working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User info successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
