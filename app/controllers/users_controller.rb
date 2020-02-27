a5ece3cd8490624e6da248bc0efa16cfdde9e4a6
deelye <dee.yen.lye@gmail.com>
2020-02-27 13:40:37 +0000

    still trying to add username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       redirect_to user_path(current_user)
     else
       render :new
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :email,  :password)
   end
 
 end
